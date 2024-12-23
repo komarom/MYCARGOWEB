81f452b033adbaebf7cfbbb219fd90d0fd0c452a
komarom <84075674+komarom@users.noreply.github.com>
2024-12-23 13:40:06 +0900

    테스트

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,12 +2,24 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? 'http://27.71.17.99:9090'
+    ? ''
     : '/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
 });
+
+api.interceptors.request.use(
+  config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.url = `/auth${config.url}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default api; 
