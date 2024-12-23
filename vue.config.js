031cc0ec6d5f67d53e842e812433f1b0bb17cb0a
komarom <84075674+komarom@users.noreply.github.com>
2024-12-23 11:08:01 +0900

    커밋테스트

--- vue.config.js
+++ vue.config.js
@@ -6,18 +6,7 @@ module.exports = defineConfig({
       '/auth': {
         target: 'http://27.71.17.99:9090',
         changeOrigin: true,
-        secure: false,
-        headers: {
-          'Host': 'www.cocodot.info',
-          'Origin': 'http://www.cocodot.info',
-          'Referer': 'http://www.cocodot.info'
-        },
-        onProxyReq: function(proxyReq) {
-          proxyReq.setHeader('Origin', 'http://www.cocodot.info');
-        },
-        onProxyRes: function(proxyRes) {
-          proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-        }
+        secure: false
       }
     }
   }
