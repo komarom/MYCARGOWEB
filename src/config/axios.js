fc0a317163780159668419afc7d046a19e7fa39a
komarom <84075674+komarom@users.noreply.github.com>
2024-12-27 17:58:26 +0900

    테스트

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://27.71.17.99:9090'
-axios.defaults.headers.common['Content-Type'] = 'application/json'
+const instance = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
 
-export default axios
+export default instance
