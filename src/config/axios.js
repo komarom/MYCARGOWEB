23292d5ded67e56408c9bd3d99ccf49d1ea9b7e8
komarom <84075674+komarom@users.noreply.github.com>
2025-01-03 14:37:49 +0900

    vercel 테스트

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://27.71.17.99:9090'
+  baseURL: process.env.VUE_APP_API_URL || 'http://27.71.17.99:9090'
 })
 
-// Rest of your axios configuration code...
 export default instance
